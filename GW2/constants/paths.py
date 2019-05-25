c6f4b4f7e0b0efe7eff7aedccc7295f4c7f289fb
Mefodii <mefodii04@gmail.com>
2019-05-25 11:19:03 +0300

    GW2 moved to separate project

--- GW2/constants/paths.py
+++
@@ -1,8 +0,0 @@
-import os
-
-PROJECT_PATH = "\\".join(os.getcwd().split("\\")[0:-1])
-FILES_PATH = PROJECT_PATH + "\\files"
-OUTPUT_FILES_PATH = FILES_PATH + "\\output"
-
-INPUT_FILE = "input.txt"
-INPUT_FILE_PATH = FILES_PATH + "\\" + INPUT_FILE
