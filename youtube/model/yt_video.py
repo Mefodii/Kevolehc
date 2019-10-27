72137d22cbcb627e8703c828005a95ad368236a0
Mefodii <mefodii04@gmail.com>
2019-10-27 09:43:29 +0200

    - Parametrical constructor - yt_result_json is not processed in separate function

--- youtube/model/yt_video.py
+++ youtube/model/yt_video.py
@@ -3,14 +3,34 @@ from ..utils.file_names import replace_unicode_chars
 
 
 class YoutubeVideo:
-    def __init__(self, json_data, video_number):
-        self.id = json_data.get("snippet").get("resourceId").get("videoId")
-        self.title = replace_unicode_chars(normalize('NFC', json_data.get("snippet").get("title")))
-        self.publishedAt = json_data.get("snippet").get("publishedAt")
-        self.channel_name = json_data.get("snippet").get("channelTitle")
-        self.number = video_number
-        self.file_name = ""
-        self.save_location = ""
+
+    ID = "ID"
+    TITLE = "TITLE"
+    PUBLISHED_AT = "PUBLISHED_AT"
+    CHANNEL_NAME = "CHANNEL_NAME"
+    NUMBER = "NUMBER"
+    FILE_NAME = "FILE_NAME"
+    SAVE_LOCATION = "SAVE_LOCATION"
+
+    def __init__(self, id, title, published_at, channel_name, number, file_name="", save_location=""):
+        self.id = id
+        self.title = title
+        self.publishedAt = published_at
+        self.channel_name = channel_name
+        self.number = number
+        self.file_name = file_name
+        self.save_location = save_location
+
+    @staticmethod
+    def parse_json_yt_response(json_data):
+        params = {
+            YoutubeVideo.ID: json_data.get("snippet").get("resourceId").get("videoId"),
+            YoutubeVideo.TITLE: replace_unicode_chars(normalize('NFC', json_data.get("snippet").get("title"))),
+            YoutubeVideo.PUBLISHED_AT: json_data.get("snippet").get("publishedAt"),
+            YoutubeVideo.CHANNEL_NAME: json_data.get("snippet").get("channelTitle"),
+        }
+        return params
 
     def __str__(self):
-        return {"Id": self.id, "Number": self.number, "Title": self.title, "PublisedAt": self.publishedAt}
+        return {YoutubeVideo.ID: self.id, YoutubeVideo.NUMBER: self.number, YoutubeVideo.TITLE: self.title,
+                YoutubeVideo.PUBLISHED_AT: self.publishedAt}
