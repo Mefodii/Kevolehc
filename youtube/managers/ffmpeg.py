4948cf4b1d0bea11e96b45cb9806fda2fc8f1c29
Mefodii <mefodii04@gmail.com>
2019-10-27 09:46:17 +0200

    - Added new function: read metadata - Added new function: convert read metada to json

--- youtube/managers/ffmpeg.py
+++ youtube/managers/ffmpeg.py
@@ -1,10 +1,11 @@
 import os
+import re
 
+from utils import File
 from ..utils.constants import MERGED_FORMAT
 
-
+METADATA_HEADER = ";FFMETADATA1"
 class Ffmpeg:
-
     @staticmethod
     def merge_audio_and_video(files_path, audio, video, merged):
         audio_abs_path = files_path + "\\" + video
@@ -43,3 +44,48 @@ class Ffmpeg:
 
         os.rename(tag_abs_path, file_abs_path)
         os.remove(temp_abs_path)
+
+    @staticmethod
+    def get_metadata(file_abs_path):
+        file_format = file_abs_path.split(".")[-1]
+        file_path = "\\".join(file_abs_path.split("\\")[:-1])
+
+        temp_abs_path = file_path + "\\tempmeta." + file_format
+        temp_metadata_file = file_path + "\\tempmetadata.txt"
+
+        os.rename(file_abs_path, temp_abs_path)
+
+        read_metadata_command = "ffmpeg" \
+                                " -i " + temp_abs_path + \
+                                " -f ffmetadata " + temp_metadata_file
+
+        os.system(read_metadata_command)
+        os.rename(temp_abs_path, file_abs_path)
+
+        metadata = File.get_file_lines(temp_metadata_file, "8")
+        os.remove(temp_metadata_file)
+        return metadata
+
+    @staticmethod
+    def metadata_to_json(raw_metadata):
+        json_metadata = {}
+        attr_match = "\\S*="
+        attr_name = None
+        attr_value = None
+
+        for line in raw_metadata:
+            parsed_line = re.search(attr_match, line)
+            if line == METADATA_HEADER:
+                pass
+            else:
+                if parsed_line and len(parsed_line.group()) > 1:
+                    if attr_name:
+                        json_metadata[attr_name] = attr_value
+
+                    attr_name = parsed_line.group().replace("=", "").upper()
+                    attr_value = "=".join(line.split("=")[1:])
+                else:
+                    attr_value += "\n" + line
+        json_metadata[attr_name] = attr_value
+
+        return json_metadata
