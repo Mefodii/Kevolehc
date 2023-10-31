67651c7c27f8a77b7f0a095446843022d0ff2d97
Mefodii <mefodii04@gmail.com>
2023-10-31 20:23:50 +0200

    use url attribute instead of video_id

--- youtube/model/playlist_item.py
+++ youtube/model/playlist_item.py
@@ -12,17 +12,20 @@ ITEM_FLAG_MISSING = "[@]"
 
 
 class PlaylistItem:
-    def __init__(self, title: str, track_nr: int, video_id: str, item_flag: str):
+    def __init__(self, title: str, track_nr: int, url: str, item_flag: str):
         self.title = title
         self.track_nr = track_nr
-        self.video_id = video_id
+        self.url = url
         self.item_flag = item_flag
+        # Note: currently no special handling for children.
+        # Every line which is under this item and until next PlaylistItem is considered as child.
         self.children: list[str] = []
 
     @classmethod
     def from_youtubevideo(cls, item: YoutubeVideo):
         item_flag = ITEM_FLAG_DEFAULT if File.exists(item.get_file_abs_path()) else ITEM_FLAG_MISSING
-        obj = PlaylistItem(item.title, item.number, item.video_id, item_flag)
+        url = DEFAULT_YOUTUBE_WATCH + item.video_id
+        obj = PlaylistItem(item.title, item.number, url, item_flag)
         return obj
 
     @classmethod
@@ -37,6 +40,6 @@ class PlaylistItem:
     def __str__(self):
         s = "".ljust(START_POS_ITEM_FLAG - 1) + self.item_flag
         s = s.ljust(START_POS_TITLE - 1) + self.title
-        s = s.ljust(START_POS_URL - 1) + DEFAULT_YOUTUBE_WATCH + self.video_id
+        s = s.ljust(START_POS_URL - 1) + self.url
         s = s.ljust(START_POS_TRACK_NR - 1) + str(self.track_nr)
         return s
