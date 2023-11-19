f514156a955b1ad5446fe04b811bd501f44eb899
Mefodii <mefodii04@gmail.com>
2023-11-19 22:32:04 +0200

    read / write objects from file

--- youtube/watchers/youtube/media.py
+++ youtube/watchers/youtube/media.py
@@ -1,5 +1,8 @@
+from typing import Self
+
 from unicodedata import normalize
 
+from utils import file
 from youtube.model.file_extension import FileExtension
 from youtube.utils.file_names import replace_unicode_chars, normalize_file_name
 from youtube.watchers.youtube.watcher import YoutubeWatcher
@@ -23,9 +26,10 @@ class YoutubeVideo:
     STATUS_UNABLE = "UNABLE"
     STATUS_DOWNLOADED = "DOWNLOADED"
     STATUS_MISSING = "MISSING"
+    STATUS_SKIP = "SKIP"
 
     def __init__(self, video_id: str, title: str, channel_name: str, published_at: str, number: int,
-                 save_location: str, file_extension: FileExtension = None, file_name: str = None,
+                 save_location: str = None, file_extension: FileExtension = None, file_name: str = None,
                  video_quality: int = None, status: str = None):
         self.video_id = video_id
         self.title = replace_unicode_chars(normalize('NFC', title))
@@ -44,10 +48,10 @@ class YoutubeVideo:
             self.status = status
 
     @classmethod
-    def from_youtube_api_video_and_watcher(cls, item: YoutubeAPIVideo, watcher: YoutubeWatcher):
+    def from_youtube_api_video_and_watcher(cls, item: YoutubeAPIVideo, watcher: YoutubeWatcher) -> Self:
         video_id = item.get_id()
         title = item.get_title()
-        channel_name = item.get_channel_name()
+        channel_name = watcher.name
         published_at = item.get_publish_date()
 
         obj = cls(video_id, title, channel_name, published_at,
@@ -56,21 +60,35 @@ class YoutubeVideo:
         return obj
 
     @classmethod
-    def from_dict(cls, data: dict):
+    def from_dict(cls, data: dict) -> Self:
         video_id = data.get(YoutubeVideo.VIDEO_ID)
         title = data.get(YoutubeVideo.TITLE)
         channel_name = data.get(YoutubeVideo.CHANNEL_NAME)
         published_at = data.get(YoutubeVideo.PUBLISHED_AT)
         number = data.get(YoutubeVideo.NUMBER)
-        save_location = data.get(YoutubeVideo.SAVE_LOCATION)
         file_extension = FileExtension.from_str(data.get(YoutubeVideo.FILE_EXTENSION))
         file_name = data.get(YoutubeVideo.FILE_NAME)
         video_quality = data.get(YoutubeVideo.VIDEO_QUALITY)
         status = data.get(YoutubeVideo.STATUS)
 
-        obj = cls(video_id, title, channel_name, published_at, number, save_location,
+        obj = cls(video_id, title, channel_name, published_at, number, save_location=None,
                   file_extension=file_extension, file_name=file_name, video_quality=video_quality, status=status)
         return obj
+
+    @classmethod
+    def from_db_file(cls, db_file: str) -> dict[str, Self]:
+        db_data = file.read_json(db_file)
+        videos = {}
+        for k in db_data:
+            video = YoutubeVideo.from_dict(k)
+            videos[video.video_id] = video
+        return videos
+
+    @classmethod
+    def write(cls, db_file: str, videos: dict[str, Self]) -> None:
+        videos_list: list[YoutubeVideo] = list(sorted(videos.values(), key=lambda v: v.number))
+        db_data = [v.to_dict() for v in videos_list]
+        file.write_json(db_file, db_data)
 
     def init_file_name(self) -> None:
         file_name = self.file_name
@@ -93,7 +111,6 @@ class YoutubeVideo:
             YoutubeVideo.CHANNEL_NAME: self.channel_name,
             YoutubeVideo.PUBLISHED_AT: self.published_at,
             YoutubeVideo.NUMBER: self.number,
-            YoutubeVideo.SAVE_LOCATION: self.save_location,
             YoutubeVideo.FILE_EXTENSION: self.file_extension.value,
             YoutubeVideo.FILE_NAME: self.file_name,
             YoutubeVideo.VIDEO_QUALITY: self.video_quality,
@@ -105,8 +122,13 @@ class YoutubeVideo:
         return data
 
     def __str__(self):
-        return str({YoutubeVideo.VIDEO_ID: self.video_id, YoutubeVideo.NUMBER: self.number,
-                    YoutubeVideo.TITLE: self.title, YoutubeVideo.PUBLISHED_AT: self.published_at})
+        return str(self.to_dict())
 
     def __repr__(self):
         return str(self)
+
+
+class YoutubeVideoList(list):
+    # TODO (very low priority) - try to extend list functionality to mimic db_utils.py
+    # https://realpython.com/inherit-python-list/#creating-list-like-classes-in-python
+    pass
