cd1b885b519bc4904127fc798a6f31dad8b90caf
Mefodii <mefodii04@gmail.com>
2023-12-05 17:12:41 +0200

    updated comics

--- comics_namer/main.py
+++ comics_namer/main.py
@@ -1,9 +1,10 @@
 import time
 import os
 from datetime import datetime
+from typing import Self
 
 from utils import file
-from utils.file import list_files, CR_TIME, FILENAME, PATH, EXTENSION
+from utils.file import list_files, File
 from constants.paths import DB_FILE
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
@@ -18,59 +19,89 @@ COMIC_PAGES_NR = "pages"
 COMIC_CREATION_DATE = "creation_date"
 COMIC_MOD_DATE = "modification_date"
 
-C_THE_CUMMONER = "The Cummoner"
-C_OGLAF = "Oglaf"
-C_CASSIOPEIA_QUINN = "Cassiopeia Quinn"
-C_IROVEDOUT = "I Roved Out"
-C_ALFIE = "Alfie"
+
+class Comic:
+    def __init__(self, name: str, path: str, pages: int, cr_date: str, mod_date: str):
+        self.name = name
+        self.path = path
+        self.pages = pages
+        self.cr_date = cr_date
+        self.mod_date = mod_date
+
+    @classmethod
+    def new(cls, comic_name: str, comic_path: str) -> Self:
+        new_date = get_current_date()
+        return cls(comic_name, comic_path, 0, new_date, new_date)
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Self:
+        name = data.get(COMIC_NAME)
+        path = data.get(COMIC_PATH)
+        pages = data.get(COMIC_PAGES_NR)
+        cr_date = data.get(COMIC_CREATION_DATE)
+        mod_date = data.get(COMIC_MOD_DATE)
+
+        obj = cls(name, path, pages, cr_date, mod_date)
+        return obj
+
+    def to_dict(self):
+        data = {
+            COMIC_NAME: self.name,
+            COMIC_PATH: self.path,
+            COMIC_PAGES_NR: self.pages,
+            COMIC_CREATION_DATE: self.cr_date,
+            COMIC_MOD_DATE: self.mod_date,
+        }
+
+        return data
+
+    def sync_pages_name(self, keep_old_name=True):
+        files = list_files(self.path, with_creation_time=True)
+        # To change creation date in powershell use command:
+        #   (Get-Item "C:\Users\mefod\Downloads\p3-2.jpg").CreationTime=("21 April 2022 07:00:02")
+        files = sorted(files, key=lambda f: f.cr_time)
+
+        new_pages = len(files)
+        if self.pages == new_pages:
+            print(f"{self.name} - has no new pages. Skipped")
+            return
+
+        if self.pages > new_pages:
+            msg = (f"{self.name} - unable to sync. Comic has pages value higher than total files len. "
+                   f"Pages: {self.pages}. Files: {new_pages}")
+            raise Exception(msg)
+
+        rename_files(files[self.pages:], self.name, self.pages + 1, keep_old_name)
+
+        print(f"{self.name} - added {new_pages - self.pages} pages")
+        self.pages = len(files)
+        self.mod_date = get_current_date()
+
+
+class ComicDict:
+
+    def __init__(self, data: dict[str, Comic]):
+        self.comics = data
+
+    @classmethod
+    def from_file(cls, file_path: str) -> Self:
+        data = file.read_json(file_path)
+        comics = [Comic.from_dict(v) for v in data]
+        return cls({comic.name: comic for comic in comics})
+
+    def write(self, file_path: str) -> None:
+        data = [v.to_dict() for k, v in self.comics.items()]
+        file.write_json(file_path, data)
+
+    def add_new(self, comic_name: str, comic_path: str):
+        if self.comics.get(comic_name):
+            raise Exception(f"Comic with name: {comic_name} already exists. Unable to insert")
+
+        self.comics[comic_name] = Comic.new(comic_name, comic_path)
 
 
 def get_current_date():
     return datetime.utcnow().strftime(DATE_FORMAT)[:-3]+"Z"
-
-
-def add_new_comic(comic_path, comic_name):
-    db_json = {}
-    if file.exists(DB_FILE):
-        db_json = file.read_json(DB_FILE)
-
-    if db_json.get(comic_name, None):
-        raise Exception(f"Comic with name: {comic_name} already exists. Unable to insert")
-
-    db_json[comic_name] = {
-        COMIC_NAME: comic_name,
-        COMIC_PATH: comic_path,
-        COMIC_PAGES_NR: 0,
-        COMIC_CREATION_DATE: get_current_date(),
-        COMIC_MOD_DATE: get_current_date(),
-    }
-
-    file.write_json(DB_FILE, db_json)
-
-
-def sync_pages_name(comic_name, keep_old_name=True):
-    if not file.exists(DB_FILE):
-        raise Exception(f"DB_FILE does not exist: {DB_FILE}")
-
-    db_json = file.read_json(DB_FILE)
-    if not db_json.get(comic_name, None):
-        raise Exception(f"Comic with name: {comic_name} does not exist.")
-
-    comic = db_json.get(comic_name)
-    comic_path = comic.get(COMIC_PATH)
-    pages_nr = comic.get(COMIC_PAGES_NR)
-
-    files = list_files(comic_path, with_creation_time=True)
-    # To change creation date in powershell use command:
-    #   (Get-Item "C:\Users\mefod\Downloads\p3-2.jpg").CreationTime=("21 April 2022 07:00:02")
-    files = sorted(files, key=lambda f: f.cr_time)
-
-    rename_files(files[pages_nr:], comic_name, pages_nr + 1, keep_old_name)
-
-    comic[COMIC_PAGES_NR] = pages_nr
-    comic[COMIC_MOD_DATE] = get_current_date()
-
-    file.write_json(DB_FILE, db_json)
 
 
 def rename_temp_files(comic_name, initial_page_number=1, keep_old_name=True):
@@ -81,16 +112,16 @@ def rename_temp_files(comic_name, initial_page_number=1, keep_old_name=True):
     rename_files(files, comic_name, initial_page_number, keep_old_name)
 
 
-def rename_files(files, comic_name, initial_page_number, keep_old_name=True):
+def rename_files(files: list[File], comic_name: str, initial_page_number: int, keep_old_name=True):
     pages_nr = initial_page_number
     for item in files:
-        path = item[PATH]
-        old_name = item[FILENAME]
+        path = item.path
+        old_name = item.name
         new_name = f"{comic_name} - {pages_nr:04d}"
         if keep_old_name:
             new_name = f"{new_name} - {old_name}"
         else:
-            new_name += f".{item[EXTENSION]}"
+            new_name += f".{item.extension}"
 
         old_abs_name = "\\".join([path, old_name])
         new_abs_name = "\\".join([path, new_name])
@@ -100,18 +131,21 @@ def rename_files(files, comic_name, initial_page_number, keep_old_name=True):
         pages_nr += 1
 
 
+def sync_db():
+    comics_dict = ComicDict.from_file(DB_FILE)
+    comics = comics_dict.comics
+    [c.sync_pages_name() for c in comics.values()]
+    comics_dict.write(DB_FILE)
+
+
 #######################################################################################################################
 # Main function
 #######################################################################################################################
 def __main__():
     # add_new_comic(COMICS_PATH + "/Alfie [raw]", C_ALFIE)
     # sync_pages_name(C_IROVEDOUT)
-    # sync_pages_name(C_OGLAF)
-    # sync_pages_name(C_CASSIOPEIA_QUINN)
-    # sync_pages_name(C_THE_CUMMONER)
-    # sync_pages_name(C_ALFIE)
     # rename_temp_files("Brandon Sanderson - White Sand 3", 1, False)
-    rename_temp_files("Brandon Sanderson - Dark One 1", 1, False)
+    sync_db()
     pass
 
 
